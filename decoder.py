dc7d8b6f0227598bcd4d4e0803b28b104f159658
GurvanR <131609700+GurvanR@users.noreply.github.com>
2024-03-25 10:13:56 +0100

    Add files via upload

--- decoder.py
+++ decoder.py
@@ -1,10 +1,50 @@
 import torch.nn as nn
 
-# Ajouter padding_mode = 'reflect' and "nearest upsampling" for this decoder ?
+
 
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
+        self.decoder = nn.Sequential(
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 256, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 128, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 64, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 3, (3, 3)),
+    )
+
+    #def forward(self, x):
+    #    return self.decoder(x)
+    def forward(self, x):
+        return self.decoder(x)
+    
+class DecoderOLD(nn.Module):
+    def __init__(self):
+        super(DecoderOLD, self).__init__()
         self.decoder = nn.Sequential(
             nn.ReflectionPad2d((1, 1, 1, 1)),
             nn.Conv2d(512, 256, (3, 3)),
